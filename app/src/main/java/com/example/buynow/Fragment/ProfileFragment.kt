7a37720511d6374f0989609ea29d6ba2e20a87b9
Jahid Hasan <vdjsovaj@gmail.com>
2021-06-02 14:28:06 +0600

    bug fix

--- app/src/main/java/com/example/buynow/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/buynow/Fragment/ProfileFragment.kt
@@ -1,23 +1,47 @@
 package com.example.buynow.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+
 import com.example.buynow.R
+import com.example.buynow.SettingsActivity
+import de.hdodenhof.circleimageview.CircleImageView
 
 
 class ProfileFragment : Fragment() {
 
+    lateinit var profileImage_profileFrag: CircleImageView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_profile, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        profileImage_profileFrag = view.findViewById(R.id.profileImage_profileFrag)
+        val settingCd_profileFrag = view.findViewById<CardView>(R.id.settingCd_profileFrag)
+
+        settingCd_profileFrag.setOnClickListener {
+            val intent = Intent(context, SettingsActivity::class.java)
+            startActivity(intent)
+        }
 
 
+        profileImage_profileFrag.setOnClickListener {
+
+        }
+
+        return view
     }
 
 }
---
+++ app/src/main/java/com/example/buynow/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.buynow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
