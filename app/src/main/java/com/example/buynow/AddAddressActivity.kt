f01f8f3558f7d8c05ca6c0028f9cdb72861af7c0
Jahid hasan <vdjsovaj@gmail.com>
2021-07-07 15:49:19 +0600

    update

--- app/src/main/java/com/example/buynow/AddAddressActivity.kt
+++ app/src/main/java/com/example/buynow/AddAddressActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class AddAddressActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_address)
